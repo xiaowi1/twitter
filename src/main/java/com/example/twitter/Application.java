0cd0e319d8d6f9ff199c84f4b231d4f20e3b8e1b
Patryk <szwaj.pat@gmail.com>
2020-01-21 22:06:20 +0100

    rename application class

--- src/main/java/com/example/twitter/Application.java
+++ src/main/java/com/example/twitter/Application.java
@@ -4,12 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TwitterApplication {
+public class Application {
 
 	public static void main(String[] args) {
 
 
-		SpringApplication.run(TwitterApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
