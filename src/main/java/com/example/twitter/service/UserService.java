bad537f48ced78706007b65f2eee9b43915dd4af
Patryk <szwaj.pat@gmail.com>
2020-01-21 22:16:19 +0100

    refactor userService

--- src/main/java/com/example/twitter/service/UserService.java
+++ src/main/java/com/example/twitter/service/UserService.java
@@ -3,31 +3,28 @@ package com.example.twitter.service;
 import com.example.twitter.model.Posting;
 import com.example.twitter.model.User;
 import com.example.twitter.repository.UserRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@AllArgsConstructor
 public class UserService {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
-    private UserRepository userRepository;
-    private PasswordEncoder passwordEncoder;
-
-    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder){
-this.userRepository = userRepository;
-this.passwordEncoder = passwordEncoder;
-}
-    public void saveUser(User user){
+    public void saveUser(User user) {
         user.setPassword(passwordEncoder.encode(user.getPassword()));
         userRepository.save(user);
     }
 
-    public void savePost(Posting post){
+    public void savePost(Posting post) {
 
     }
 
-    public List<User> getAllUsers(){
+    public List<User> getAllUsers() {
         return userRepository.findAll();
     }
 }
