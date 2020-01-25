996bf080ae436155f467cf32f79ae0c3bc550f3e
Patryk <szwaj.pat@gmail.com>
2020-01-25 12:10:46 +0100

    refactor index, added posts to front page

--- src/main/java/com/example/twitter/controllers/IndexController.java
+++ src/main/java/com/example/twitter/controllers/IndexController.java
@@ -1,21 +1,32 @@
 package com.example.twitter.controllers;
 
+import com.example.twitter.model.Posting;
+import com.example.twitter.service.PostingService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpSession;
+import java.util.List;
 
 @Controller
 public class IndexController {
 
+    @Autowired
+    private PostingService postingService;
+
+
     @RequestMapping(value = {"/", "/index"})
-    public String index(Authentication authentication, HttpSession session){
+    public ModelAndView index(Authentication authentication, HttpSession session){
+        List<Posting> posts = postingService.getAllPosts();
         String login=authentication.getName();
         session.setAttribute("login",login);
-        return "index";
+        return new ModelAndView("index", "posts", posts);
     }
 
     @GetMapping("/login")
